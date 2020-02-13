2c2085b17c2a481bb6d69dfa63a9fb44ea68f570
jieyu <jieyu@jieyu-osx.corp.adobe.com>
2020-02-13 09:04:30 -0800

    get package name from package.json

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -12,16 +12,18 @@ governing permissions and limitations under the License.
 const execa = require('execa')
 const chalk = require('chalk')
 const { stdout } = require('stdout-stderr')
+const fs = require('fs')
 
 stdout.print = true 
 
 test('PhoneGap Build test', async () => {
 
-  const name = 'aio-cli-plugin-console'
+  const packagejson = JSON.parse(fs.readFileSync('package.json').toString())
+  const name = `${packagejson.name}`
   console.log(chalk.blue(`> e2e tests for ${chalk.bold(name)}`))
 
   console.log(chalk.dim(`    - PhoneGap Build..`))
-  execa.sync('./bin/run', ['pgb'], { stderr: 'inherit' })
+  expect(() => { execa.sync('./bin/run', ['pgb'], { stderr: 'inherit' }) }).not.toThrow()
 
   console.log(chalk.green(`    - done for ${chalk.bold(name)}`))
 });
